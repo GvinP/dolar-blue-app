3b934f8a49bcab9d71d086b8a57f8908d65fbe6f
kudryashovalexey <gvinpin72@gmail.com>
2025-01-19 23:43:33 +0300

    add colors

---
+++ assets/colors/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  backgroundPrimary: '#1A2039',
+  backgroundSecondary: '#595D61',
+  textAccent: '#F7A600',
+  textPrimary: '#FFF6E4',
+  positive: '#20B26C',
+  negative: '#EF454A',
+};
