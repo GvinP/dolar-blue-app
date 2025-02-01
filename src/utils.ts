af435384f5a93c5205c32addfa069b94c0956fb3
GvinP <gvinpin72@gmail.com>
2025-02-01 22:38:27 +0300

    fix chart labels (#10)

--- src/utils.ts
+++ src/utils.ts
@@ -93,3 +93,22 @@ export const removeDuplicates = (arr: Cotizacion[]): Cotizacion[] => {
     return true; // Not a duplicate, so keep it
   });
 };
+
+export const calculateLabelIndexes = (
+  dataLength: number,
+  labelCount = 7,
+): number[] => {
+  if (dataLength <= labelCount) {
+    return Array.from({length: dataLength}, (_, i) => i);
+  }
+
+  const interval = (dataLength - 1) / (labelCount - 1);
+  const indexes = [];
+
+  for (let i = 0; i < labelCount; i++) {
+    const index = Math.round(i * interval);
+    indexes.push(index);
+  }
+
+  return indexes;
+};
